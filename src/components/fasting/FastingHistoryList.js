87d3f58f12a9192b5450fe808ea878a8f823247f
Nasir Khan <rnakhan@gmail.com>
2018-11-18 19:19:52 -0800

    History List

---
+++ src/components/fasting/FastingHistoryList.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import ListItemText from '@material-ui/core/ListItemText';
+import { formatMealtime, msToTime } from '../common/Utils';
+
+export default class FastingHistoryList extends Component {
+  
+  render() {
+    return (
+      <List style={{ overflowY: "scroll" }}>
+        <ListSubheader>Recent meals</ListSubheader>
+        {this.formatListItems(this.props.fastingHistoryList)}
+      </List>
+    );
+  }
+
+  onListItemClick = (entry) => {
+    this.props.handleDialogOpen(entry);
+  }
+
+  formatListItems = (list) => {
+    list.sort( (a, b) => { return (a.date - b.date) }); // ascending
+    let newList = [];
+    let lastDate = null;
+    let fastedDuration = null;
+    let fastedDurationText = '';
+    for (let i = 0; i < list.length; i++) {
+      if (lastDate) {
+        fastedDuration = msToTime(list[i].date - lastDate);
+        fastedDurationText = 'Fasted ' + fastedDuration.hours + ':' + fastedDuration.minutes;
+      }
+      newList.push((
+        <ListItem key={list[i].date} onClick={(e) => { this.onListItemClick(list[i]) }}> 
+          <ListItemText primary={formatMealtime(list[i].date)} secondary={fastedDurationText}/>
+        </ListItem>
+      ))
+      lastDate = list[i].date;
+    }
+    return newList.reverse();
+  }
+}
